7a9a420fcadfd6f11955ae41b1e07756c1a43da0
briansimiyuj <briansimiyuj@gmail.com>
2021-02-17 09:37:32 +0300

    Time distance

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const countdown = document.querySelector('.countdown')
 
 const launchDate = new Date('March 14, 2021 00:00:00').getTime()
+
+
+const update = setInterval(() => {
+
+    const now = new Date().getTime()
+    
+    let distance = launchDate - now
+
+    console.log(distance)
+
+})
